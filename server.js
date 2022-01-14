99c7da344242cc0023b9bd4c357d01d63e0ed5a7
unknown <pranitat901@gmail.com>
2022-01-14 12:11:24 +0530

    ESLint and Prettier

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
+dotenv.config({ path: './config.env' });
 const app = require('./app');
-
 //Environment Variable
 // console.log(app.get('env')); //Set by Express
 //console.log(process.env);//Set by Nodejs
 const port = process.env.PORT || 3000;
-app.listen(port, () =>{
-    console.log(`App running at ${port}`);
+app.listen(port, () => {
+  console.log(`App running at ${port}`);
 });
